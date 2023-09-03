c6d6c40f53db8b3b3077ebe9e6b44c2b997ef886
egor_nazyrov <egop2005@mail.ru>
2023-09-03 19:14:49 +0300

    telegramBot created

---
+++ main.py
@@ -0,0 +1,4 @@
+
+
+# iamToken: t1.9euelZrOk8qMlpbIipHGj42XyZOWk-3rnpWalYnHx4zHxpOMj57Gm5nGjo_l8_cGMi1Y-e89fUEz_N3z90ZgKlj57z19QTP8zef1656VmpqdjZnOko7OmszKj5uSicaO7_zF656VmpqdjZnOko7OmszKj5uSicaO.lGtoIIEbAtBpMRrwmjF1ezkx-j4DIGK9pLDJEpi13xs2Ih7jfkON1LVnKYX14xDjxAbBZ5Jc_5KlWK1X74EGAA"
+# curl -d "{\"yandexPassportOauthToken\":\"y0_AgAAAABVFdNAAATuwQAAAADrypItR4AjRzDOQ06oHq5qPSvtgmrcH-M\"}" "https://iam.api.cloud.yandex.net/iam/v1/tokens"
